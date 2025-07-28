bf6f013e456fe816487e010055475f120fba23c0
colinmatthews <matthews.colin22@gmail.com>
2025-07-28 17:39:18 -0400

    Revert "Create Component Library PRD"
    This reverts commit 94628efe7ca865a166da097733168c56a7d36bb6.

--- src/components/Sidebar/index.ts
+++
@@ -1,4 +0,0 @@
-export { Sidebar } from './Sidebar';
-export { SidebarItem } from './SidebarItem';
-export { SidebarAccordion } from './SidebarAccordion';
-export type { SidebarProps, SidebarItemProps, SidebarAccordionProps } from '../../types/navigation';
